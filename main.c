981fe4b222f485a61dbc71ca45ad0d3fc42006b2
elpapi <andrecanelaskaldeich@gmail.com>
2023-08-02 22:45:27 +0100

    details

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: acanelas <acanelas@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 23:29:55 by acanelas          #+#    #+#             */
-/*   Updated: 2023/08/02 22:24:56 by acanelas         ###   ########.fr       */
+/*   Updated: 2023/08/02 22:44:12 by acanelas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,9 @@ int	main(int ac, char **av)
 	t_args	args;
 
 	if ((ac != 5 && ac != 6) || check_input(av) == false)
-	{
 		return (error_message());
-	}
 	if (!init_args(&args, av))
-	{
 		return (error_message());
-	}
 	else
 	{
 		printf("The simulation has started\n");
